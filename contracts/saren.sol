10242b74b6f1fd63f207af8992f6f661da8271ec
jaykch <jaykch@outlook.com>
2021-03-31 07:39:53 +1100

    Reverted contract back to github import (As deployed on mainnet)

--- contracts/saren.sol
+++ contracts/saren.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract SARToken is ERC20 {
     constructor() public ERC20("Saren", "SAR") {
